b0ff8096c0a442297fd2947fcf8203f8508be6af
Talib Sheikh <92061094+Talibshaib@users.noreply.github.com>
2023-05-22 20:16:56 +0530

    profile function added

---
+++ script.js
@@ -0,0 +1,10 @@
+let profileOption = document.getElementById("profileOption");
+
+const openProfileOption = () => {
+  console.log("haa working");
+  if (profileOption.style.display == "none") {
+    profileOption.style.display = "block";
+  } else {
+    profileOption.style.display = "none";
+  }
+};
